09cb4827961884430c70f6bc8900eb17ae7edeb5
GliGli <sfx.gligli@free.fr>
2019-11-17 20:28:28 +0100

    Wooooot! dump done in extremis :D

--- 6801_keyer/append.bat
+++ 6801_keyer/append.bat
@@ -3,3 +3,4 @@ echo [objects]>linkfile
 echo sdump.o>>linkfile
 wlalink.exe -s -r -v linkfile sdump.bin
 bin2srec -s -o -0x8000 sdump.bin > sdump.s19
+copy /b sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin+sdump.bin sdump512.bin
