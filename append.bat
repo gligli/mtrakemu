2dbc76e4d7320598ed124ed528cf5e57529400f9
GliGli <sfx.gligli@free.fr>
2019-12-18 16:22:54 +0100

    back to using $234 zone for bender patch (preparing for voice kill patch)

--- append.bat
+++ append.bat
@@ -1,4 +1,4 @@
 dd.exe if=mtrak.tpl of=mtrak.bin
-rem dd.exe if=output.sms of=mtrak.bin conv=notrunc bs=1 count=27 skip=564 seek=564
+dd.exe if=output.sms of=mtrak.bin conv=notrunc bs=1 count=27 skip=564 seek=564
 dd.exe if=output.sms of=mtrak.bin conv=notrunc bs=1 count=60 skip=13764 seek=13764
 copy /b mtrak.bin+mtrak.bin mtrak32.bin
